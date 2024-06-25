7e3530c4bef26cafdd93a0ec4cde8a494df26e7c
LuizRossini <luiz.guirossini123@gmail.com>
2024-06-25 19:26:15 -0300

    adesão da classe Calculadora e método de multiplicação [corrigido]

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Calculadora {
+//2 - Crie uma classe Calculadora com um método que recebe um número como parâmetro e retorna o dobro desse número.
+
+public class Calculadora {
+
+    public int dobro(int a) {
+        return a * 2;
+    }
+
+    public static void main(String[] args) {
+        Calculadora minhaConta = new Calculadora();
+
+        int resultado = minhaConta.dobro(10);
+        System.out.println(resultado);
+    }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,7 @@
-public class Pessoa {
+//1 - Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.
 
+public class Pessoa {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
 }
